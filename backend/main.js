ed5fd35a26745c08c820858b1cab2177848f1406
Bratnya <blankwhitte017@gmail,com>
2025-09-28 20:34:15 +0800

    Port fixed for the main.js

--- backend/main.js
+++ backend/main.js
@@ -370,7 +370,7 @@ app.post('/api/predictive-analysis/custom-options', async (req, res) => {
 });
 
 // Define the port
-const PORT = process.env.WEBSITES_PORT;
+const PORT = process.env.PORT;
 
 // Start the server
 app.listen(PORT, '0.0.0.0', () => {
